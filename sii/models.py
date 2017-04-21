2e65aead884a0f56a2b7b64b47b7b1b7e2aa9906
Gerard Dalmau <gdalmau@gisce.net>
2017-04-21 14:01:04 +0200

    Add first version models

---
+++ sii/models.py
@@ -0,0 +1,16 @@
+from marshmallow import Schema, fields
+
+
+class Cabecera(Schema):
+    nif = fields.String()
+    apellidos_nombre = fields.String()
+    tipo_comunicacion = fields.String()
+
+
+class Factura(Schema):
+    numero = fields.String()
+
+
+class XMLEnviado(Schema):
+    cabecera = fields.Nested(Cabecera)
+    factura = fields.Nested(Factura)
