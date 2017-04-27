5e34bd6eadd62b7dc706c21a9bca856fae44e26f
Gerard Dalmau <gdalmau@gisce.net>
2017-04-27 14:27:54 +0200

    Update dump to check if tax_line contains IVA

--- sii/resource.py
+++ sii/resource.py
@@ -4,7 +4,38 @@ from sii import models, __SII_VERSION__
 
 class FacturasEmitidasDictGenerator(models.SuministroFacturasEmitidas):
 
-    def generate_object(self, invoice, many=None, update_fields=True, **kwargs):
+    def dump(self, invoice, many=None, update_fields=True, **kwargs):
+        base_imponible = ''
+        for tax in invoice.tax_line:
+            if 'IVA' in tax.name:
+                base_imponible = tax.base
+                break
+        if base_imponible:
+            tipo_desglose = {
+                'DesgloseFactura': {
+                    'Sujeta': {
+                        'NoExenta': {  # TODO Exenta o no exenta??
+                            'TipoNoExenta': 'S1',  # TODO to change
+                            'DesgloseIVA': {
+                                'DetalleIVA': {
+                                    'TipoImpositivo': '',
+                                    'BaseImponible': base_imponible,
+                                    'CuotaRepercutida': '0'  # TODO
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        else:
+            tipo_desglose = {
+                'DesgloseFactura': {
+                    'NoSujeta': {
+
+                    }
+                }
+            }
+
         factura_expedida = {
             'TipoFactura': '',
             'ClaveRegimenEspecialOTrascendencia': '',
@@ -54,8 +85,10 @@ class FacturasEmitidasDictGenerator(models.SuministroFacturasEmitidas):
             raise Exception(
                 'Errors were found while trying to generate the dump', errors)
 
-        return super(models.SuministroFacturasEmitidas, self).dump(
+        res = super(models.SuministroFacturasEmitidas, self).dump(
             new_obj, many, update_fields, **kwargs)
+
+        return res
 
 
 class SII(object):
@@ -69,4 +102,5 @@ class SII(object):
         else:
             raise Exception('Error in invoice.type')
 
-        return invoice_model.generate_object(invoice).data
+        res = invoice_model.dump(invoice).data
+        return res
