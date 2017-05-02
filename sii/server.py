68d78b7562ad3ead210392945a96b44b0c512f73
MiquelIR <misernroca@gisce.net>
2017-05-02 10:31:57 +0200

    Mod server code to start creating functions

--- sii/server.py
+++ sii/server.py
@@ -1,36 +1,22 @@
 # -*- coding: UTF-8 -*-
 
 from sii.resource import SII
-import zeep
-from dicttoxml import dicttoxml
+from requests import Session
+from zeep import Client
+from zeep.transports import Transport
+from zeep.plugins import HistoryPlugin
+
 from lxml import etree, objectify
-
-
-cabecera = {
-    "SuministroLRFacturasEmitidas": {
-        "Cabecera": {
-            "IDVersionSii": "0.6",
-            "Titular": {
-                "NombreRazon": "Francisco García",
-                "NIF": "12345678T"},
-            "TipoComunicacion": "A0"
-        }
-    }
-}
-
-dict_to_xml = dicttoxml(cabecera)
-import xml.dom.minidom
-xml_pretty = xml.dom.minidom.parseString(dict_to_xml) # or xml.dom.minidom.parseString(xml_string)
-pretty_xml_as_string = xml_pretty.toprettyxml()
-
-
-with open('/home/miquel/codi/sii/sii/data/SuministroLR.xsd', 'r') as myfile:
-    xsd_read = myfile.read().replace('\n', '')
 
 with open('/home/miquel/codi/sii/sii/xml_examples/ejemplo_xml_alta_factura_emitida.xml', 'r') as myfile:
     xml_read = myfile.read().replace('\n', '')
     # from pprintpp import pprint
     # pprint(xml_da)
+
+# def get_xsd_path(self):
+#     return "{}/giscedata_switching_cnmc_reports/xml/{}".format(
+#         config['addons_path'], self.xsd_file
+#     )
 
 xmlschema = etree.XMLSchema(file='/home/miquel/codi/sii/sii/data/SuministroLR.xsd')
 
@@ -39,4 +25,15 @@ a = objectify.fromstring(xml_read, parser)
 
 wsdl_out_invoice = 'http://www.agenciatributaria.es/static_files/AEAT/Contenidos_Comunes/La_Agencia_Tributaria/Modelos_y_formularios/Suministro_inmediato_informacion/FicherosSuministros/V_06/SuministroFactEmitidas.wsdl'
 
-client = zeep.Client(wsdl=wsdl_out_invoice)
+session = Session()
+# session.cert = (publicCrt, privateKey)
+transport = Transport(session=session)
+history = HistoryPlugin()
+# client = Client(wsdl=wsdl_out_invoice, transport=transport, plugins=[history])
+client = Client(wsdl=wsdl_out_invoice, transport=transport)
+
+port_name = 'SuministroFactEmitidasPruebas'
+serv = client.bind('siiService', port_name)
+
+res = serv.SuministroLRFacturasEmitidas(header, invoices)
+
