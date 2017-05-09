6066112cf49c057a5129e695e51ec91767424d46
Gerard Dalmau <gdalmau@gisce.net>
2017-05-09 13:10:34 +0200

    Add option 'S3' for TipoNoExenta

--- sii/models.py
+++ sii/models.py
@@ -9,7 +9,7 @@ PERIODO_VALUES = [
     '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '0A'
 ]
 
-TIPO_NO_EXENTA_VALUES = ['S1', 'S2']
+TIPO_NO_EXENTA_VALUES = ['S1', 'S2', 'S3']
 
 TIPO_RECTIFICATIVA_VALUES = ['S', 'I']
 
