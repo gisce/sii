cb1ea268f09e48a2b828d53d70d95173f0dfcd96
MiquelIR <misernroca@gisce.net>
2017-05-22 11:13:47 +0200

    Add service code

--- sii/server.py
+++ sii/server.py
@@ -6,8 +6,8 @@ from requests import Session
 from zeep.wsse.signature import Signature
 from zeep.transports import Transport
 from utils import fix_ssl_verify
-from dicttoxml import dicttoxml
-from lxml import etree, objectify
+# from dicttoxml import dicttoxml
+# from lxml import etree, objectify
 
 wsdl_files = {
     'emitted_invoice': 'http://www.agenciatributaria.es/static_files/AEAT/Contenidos_Comunes/La_Agencia_Tributaria/Modelos_y_formularios/Suministro_inmediato_informacion/FicherosSuministros/V_07/SuministroFactEmitidas.wsdl',
@@ -44,10 +44,11 @@ class Service(object):
     def create_service(self, i_type):
 
         session = Session()
-        session.cert = (self.certificate, self.key)
+        # session.cert = (self.certificate, self.key)
         session.verify = False
+        session.trust_env = False
+        session.proxies = {'https': 'http://sii-proxy.gisce.net:4443'}
         transport = Transport(session=session)
-
         if i_type.startswith('out_'):
             wsdl = wsdl_files['emitted_invoice']
             port_name = 'SuministroFactEmitidasPruebas'
@@ -56,21 +57,20 @@ class Service(object):
             port_name = 'SuministroFactRecibidasPruebas'
 
         client = Client(wsdl=wsdl, port_name=port_name, transport=transport,
-                        service_name='siiService', wsse=Signature(self.key,
-                        self.certificate))
-
+                        service_name='siiService')
+        # , wsse = Signature(self.key,self.certificate)
         # if self.test_mode:
         # port_name += 'Pruebas'
-        service = client.create_service('{https://www2.agenciatributaria.gob.es/static_files/common/internet/dep/aplicaciones/es/aeat/ssii/fact/ws/SuministroFactEmitidas.wsdl}siiBinding',
-                                        'https://sii-proxy.gisce.net:4443')
+        # service = client.create_service('{https://www2.agenciatributaria.gob.es/static_files/common/internet/dep/aplicaciones/es/aeat/ssii/fact/ws/SuministroFactEmitidas.wsdl}siiBinding',
+        #                                 'https://sii-proxy.gisce.net:4443')
         # serv = client.bind('siiService', port_name)
-        return service
+        return client
 
     def send_invoice(self, invoice):
         msg_header, msg_invoice = self.get_msg(invoice)
         try:
             if invoice.type == 'out_invoice':
-                res = self.emitted_service.SuministroLRFacturasEmitidas(
+                res = self.emitted_service.service.SuministroLRFacturasEmitidas(
                     msg_header, msg_invoice)
                 if res['EstadoEnvio'] == 'Correcto':
                     self.result['sii_sent'] = True
@@ -95,6 +95,6 @@ class Service(object):
         res_header = dict_from_marsh['SuministroLRFacturasEmitidas']['Cabecera']
         res_invoices = dict_from_marsh['SuministroLRFacturasEmitidas'][
             'RegistroLRFacturasEmitidas']
-        xml_from_dict = dicttoxml(dict_from_marsh, root=False, attr_type=False)
+        # xml_from_dict = dicttoxml(dict_from_marsh, root=False, attr_type=False)
 
         return res_header, res_invoices
