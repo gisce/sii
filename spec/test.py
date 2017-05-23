9d5469b7450c791de09d38409bd958cc669e8616
MiquelIR <misernroca@gisce.net>
2017-05-23 12:21:44 +0200

    Test-Mod testing to use refunds too

--- spec/test.py
+++ spec/test.py
@@ -9,9 +9,11 @@ from spec.testing_data import DataGenerator
 data_gen = DataGenerator()
 in_invoice = data_gen.get_in_invoice()
 out_invoice = data_gen.get_out_invoice()
+in_refund = data_gen.get_in_refund_invoice()
+out_refund = data_gen.get_out_refund_invoice()
 s = Service('/home/miquel/Documents/SII/client_ssl/client.crt',
             '/home/miquel/Documents/SII/client_ssl/client.key')
-invoice = out_invoice
+invoice = out_refund
 s.send(invoice)
 
 print s.result
