dcfce3f3b9a09c1197dde896bf6894f2e34aa1e4
Gerard Dalmau <gdalmau@gisce.net>
2017-06-02 13:17:27 +0200

    Add marhsmallow fields for IDOtro
    This is when Contraparte NIF is not registered in AEAT

--- sii/models/invoices_record.py
+++ sii/models/invoices_record.py
@@ -19,6 +19,40 @@ TIPO_NO_EXENTA_VALUES = ['S1', 'S2', 'S3']
 
 TIPO_RECTIFICATIVA_VALUES = ['S', 'I']
 
+CODIGO_PAIS_VALUES = [
+    'AF', 'AL', 'DE', 'AD', 'AO', 'AI', 'AQ', 'AG', 'SA', 'DZ', 'AR', 'AM',
+    'AW', 'AU', 'AT', 'AZ', 'BS', 'BH', 'BD', 'BB', 'BE', 'BZ', 'BJ', 'BM',
+    'BY', 'BO', 'BA', 'BW', 'BV', 'BR', 'BN', 'BG', 'BF', 'BI', 'BT', 'CV',
+    'KY', 'KH', 'CM', 'CA', 'CF', 'CC', 'CO', 'KM', 'CG', 'CD', 'CK', 'KP',
+    'KR', 'CI', 'CR', 'HR', 'CU', 'TD', 'CZ', 'CL', 'CN', 'CY', 'CW', 'DK',
+    'DM', 'DO', 'EC', 'EG', 'AE', 'ER', 'SK', 'SI', 'ES', 'US', 'EE', 'ET',
+    'FO', 'PH', 'FI', 'FJ', 'FR', 'GA', 'GM', 'GE', 'GS', 'GH', 'GI', 'GD',
+    'GR', 'GL', 'GU', 'GT', 'GG', 'GN', 'GQ', 'GW', 'GY', 'HT', 'HM', 'HN',
+    'HK', 'HU', 'IN', 'ID', 'IR', 'IQ', 'IE', 'IM', 'IS', 'IL', 'IT', 'JM',
+    'JP', 'JE', 'JO', 'KZ', 'KE', 'KG', 'KI', 'KW', 'LA', 'LS', 'LV', 'LB',
+    'LR', 'LY', 'LI', 'LT', 'LU', 'XG', 'MO', 'MK', 'MG', 'MY', 'MW', 'MV',
+    'ML', 'MT', 'FK', 'MP', 'MA', 'MH', 'MU', 'MR', 'YT', 'UM', 'MX', 'FM',
+    'MD', 'MC', 'MN', 'ME', 'MS', 'MZ', 'MM', 'NA', 'NR', 'CX', 'NP', 'NI',
+    'NE', 'NG', 'NU', 'NF', 'NO', 'NC', 'NZ', 'IO', 'OM', 'NL', 'BQ', 'PK',
+    'PW', 'PA', 'PG', 'PY', 'PE', 'PN', 'PF', 'PL', 'PT', 'PR', 'QA', 'GB',
+    'RW', 'RO', 'RU', 'SB', 'SV', 'WS', 'AS', 'KN', 'SM', 'SX', 'PM', 'VC',
+    'SH', 'LC', 'ST', 'SN', 'RS', 'SC', 'SL', 'SG', 'SY', 'SO', 'LK', 'SZ',
+    'ZA', 'SD', 'SS', 'SE', 'CH', 'SR', 'TH', 'TW', 'TZ', 'TJ', 'PS', 'TF',
+    'TL', 'TG', 'TK', 'TO', 'TT', 'TN', 'TC', 'TM', 'TR', 'TV', 'UA', 'UG',
+    'UY', 'UZ', 'VU', 'VA', 'VE', 'VN', 'VG', 'VI', 'WF', 'YE', 'DJ', 'ZM',
+    'ZW'
+]
+
+ID_TYPE_VALUES = {
+    '02': u'NIF-IVA',
+    '03': u'PASAPORTE',
+    '04': u'DOCUMENTO OFICIAL DE IDENTIFICACIÓN EXPEDIDO POR EL PAIS O '
+          u'TERRITORIO DE RESIDENCIA',
+    '05': u'CERTIFICADO DE RESIDENCIA',
+    '06': u'OTRO DOCUMENTO PROBATORIO',
+    '07': u'NO CENSADO'
+}
+
 # Valores para la Clave de Régimen Especial para facturas emitidas
 CRE_FACTURAS_EMITIDAS = [
     ('01', u'Operación de régimen general'),
@@ -96,6 +130,16 @@ class MySchema(Schema):
         unknown = list(set(original_data) - set(self.fields))
         if unknown:
             raise ValidationError('Unknown field', unknown)
+
+
+class IDOtro(MySchema):
+    CodigoPais = fields.String(
+        required=True, validate=validate.OneOf(CODIGO_PAIS_VALUES)
+    )
+    IDType = fields.String(
+        required=True, validate=validate.OneOf(ID_TYPE_VALUES.keys())
+    )
+    ID = fields.String(required=True, validate=validate.Length(max=20))
 
 
 class NIF(MySchema):
@@ -195,6 +239,7 @@ class TipoDesglose(MySchema):  # TODO obligatorio uno de los dos pero sólo pued
 
 
 class Contraparte(Titular):
+    IDOtro = fields.Nested(IDOtro)
     pass
 
 
