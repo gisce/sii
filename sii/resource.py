79604ce79c0b0d299664a83a4708705358cfd2d2
Gerard Dalmau <gdalmau@gisce.net>
2017-06-15 09:25:30 +0200

    Search for 'sobre la electricidad' instead

--- sii/resource.py
+++ sii/resource.py
@@ -50,7 +50,7 @@ def get_iva_values(invoice, in_invoice):
                     iva['CuotaSoportada'] = sign * abs(inv_tax.tax_amount)
                 vals['iva_no_exento'] = True
                 vals['detalle_iva'].append(iva)
-        elif 'Impuesto especial sobre la electricidad' != inv_tax.name.lower():
+        elif 'sobre la electricidad' not in inv_tax.name.lower():
             vals['no_sujeta_a_iva'] = True
     return vals
 
