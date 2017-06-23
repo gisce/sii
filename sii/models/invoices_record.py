7047169f4f2462af518368a60d46dfce1d645897
Gerard Dalmau <gdalmau@gisce.net>
2017-06-23 11:47:17 +0200

    Add convert_camel_case_to_underscore utility func

--- sii/models/invoices_record.py
+++ sii/models/invoices_record.py
@@ -110,6 +110,11 @@ CRE_FACTURAS_RECIBIDAS = [
 ]
 
 
+def convert_camel_case_to_underscore(name):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+
 class DateString(fields.String):
     def _validate(self, value):
         if value is None:
