e12eeed93b907c4eb8939d662cdba32491016061
Gerard Dalmau <gdalmau@gisce.net>
2017-09-20 09:19:40 +0200

    Fix service operation

--- sii/server.py
+++ sii/server.py
@@ -229,10 +229,10 @@ class SiiDeregisterService(SiiService):
         msg_header, msg_invoice = self.get_deregister_msg()
         try:
             if self.invoice.type.startswith('out_'):
-                res = self.emitted_service.BajaLRFacturasEmitidas(
+                res = self.emitted_service.AnulacionLRFacturasEmitidas(
                     msg_header, msg_invoice)
             elif self.invoice.type.startswith('in_'):
-                res = self.received_service.BajaLRFacturasRecibidas(
+                res = self.received_service.AnulacionLRFacturasRecibidas(
                     msg_header, msg_invoice)
             self.result = res
             return serialize_object(self.result)
