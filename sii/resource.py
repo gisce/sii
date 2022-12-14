e8afc63b79ae8c1674901505dd1556f8cdb46748
Guillem Julià <gjulia@gisce.net>
2022-12-14 15:36:05 +0100

    sum cuota deducible from inversion sujeto pasivo

--- sii/resource.py
+++ sii/resource.py
@@ -487,14 +487,14 @@ def get_factura_recibida(invoice, rect_sust_opc1=False, rect_sust_opc2=False):
             }
         }
     if iva_values.get('inversion_sujeto_pasivo'):
-        detalle_isp = iva_values.get('inversion_sujeto_pasivo')[0]
+        detalle_isp = iva_values.get('inversion_sujeto_pasivo')
+        detalle_iva_isp = []
+        detalle_iva_isp.extend(iva_values['detalle_iva'])
+        for iva in detalle_isp:
+            cuota_deducible += iva.get('CuotaSoportada', 0)
         desglose_factura.update({
             'InversionSujetoPasivo': {
-                'DetalleIVA': [{
-                    'TipoImpositivo': detalle_isp['TipoImpositivo'],
-                    'BaseImponible': detalle_isp['BaseImponible'],
-                    'CuotaSoportada': detalle_isp['CuotaSoportada']
-                }]
+                'DetalleIVA': detalle_iva_isp
             }
         })
 
